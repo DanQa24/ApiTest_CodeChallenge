b5fe100cc334e17d53b06c167d95ebed052d10ae
Dana <bohdanna.klymiuk@gmail.com>
2021-02-13 19:17:45 +0100

    Moved test steps to separate methods - getUserIdBy & getPostsByUserId. Added Allure dependency

--- src/test/java/Tests/UsersTest.java
+++ src/test/java/Tests/UsersTest.java
@@ -1,43 +1,46 @@
 package Tests;
 
 import Config.*;
+import io.qameta.allure.*;
 import org.testng.annotations.Test;
-
 import java.util.ArrayList;
-
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 
 public class UsersTest extends TestConfig {
 
-    @Test
-    public void verifyEmailsHaveCorrectFormat() {
+    private int getUserIdBy(String username) {
+        return given()
+                .param("username", username)
+                .when()
+                .get(BlogEndpoints.USERS)
+                .then()
+                .body("size()", is(1), "[0].username", is(username))
+                .extract()
+                .path("[0].id");
+    }
+
+    private ArrayList<Integer> getPostsByUserId(int userId) {
+        return given()
+                .param("userId", userId)
+                .when()
+                .get(BlogEndpoints.POSTS)
+                .then()
+                .extract()
+                .path("id");
+    }
+
+    @Test(description = "Verify emails have valid format")
+    @Description("E2E: Verify that email format is valid in all comments under all User's posts")
+    public void verifyEmailsHaveValidFormat() {
         String username = "Delphine";
 
-        int userId =
-                given()
-                        .param("username", username)
-                .when()
-                        .get(BlogEndpoints.USERS)
-                .then()
-                        .body("size()", is(1), "[0].username", is(username))
-                        .extract()
-                        .path("[0].id");
-
-
-        ArrayList<Integer> postIds =
-                given()
-                        .param("userId", userId)
-                .when()
-                        .get(BlogEndpoints.POSTS)
-                .then()
-                        .extract()
-                        .path("id");
-
+        int userId = getUserIdBy(username);
+        ArrayList<Integer> userPosts =getPostsByUserId(userId);
 
         given()
-                .param("postId", postIds)
+                .param("postId", userPosts)
         .when()
                 .get(BlogEndpoints.COMMENTS)
         .then()
